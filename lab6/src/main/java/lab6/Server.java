0ce62d39c726e73b204707ff0e0827585d54fb1f
farichase <fbazartinova@mail.ru>
2020-12-18 20:59:12 +0300

    Scripted auto-commit on change (2020-12-18 20:59:12) by gitwatch.sh

--- lab6/src/main/java/lab6/Server.java
+++ lab6/src/main/java/lab6/Server.java
@@ -1,7 +1,9 @@
 package lab6;
 
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 
 import static akka.http.javadsl.server.Directives.*;
 
@@ -17,9 +19,9 @@ public class Server {
             parameter(URL, (url) ->
                     parameter(COUNT, count -> {
                         if (Integer.parseInt(count) <= 0){
-                            return completeWithFuture(http.singleRequest(url));
+                            return completeWithFuture(http.singleRequest(HttpRequest.create(url));
                         }
-                        return completeWithFuture()
+                        return completeWithFuture(Patterns.ask(StoreActor))
                     })
 
             )
