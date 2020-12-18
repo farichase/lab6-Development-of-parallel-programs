44667f3675ca96dd9f99e8456621563f84268851
farichase <fbazartinova@mail.ru>
2020-12-18 20:54:17 +0300

    Scripted auto-commit on change (2020-12-18 20:54:17) by gitwatch.sh

--- lab6/src/main/java/lab6/Server.java
+++ lab6/src/main/java/lab6/Server.java
@@ -1,5 +1,6 @@
 package lab6;
 
+import akka.http.javadsl.Http;
 import akka.http.javadsl.server.Route;
 
 import static akka.http.javadsl.server.Directives.*;
@@ -7,6 +8,7 @@ import static akka.http.javadsl.server.Directives.*;
 public class Server {
     private static final String URL = "url";
     private static final String COUNT = "count";
+    private static final Http http;
     public Server(){
     }
     public Route createRoute(){
@@ -14,7 +16,7 @@ public class Server {
             parameter(URL, url ->
                     parameter(COUNT, count -> {
                         if (Integer.parseInt(count) <= 0){
-                            return completeWithFuture()
+                            return completeWithFuture(http.singleRequest())
                         }
                         return completeWithFuture()
                     })
