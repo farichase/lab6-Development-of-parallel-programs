f7cfccd6fa7ef7b84147b152fd965afa6aec2deb
farichase <fbazartinova@mail.ru>
2020-12-18 21:16:54 +0300

    Scripted auto-commit on change (2020-12-18 21:16:53) by gitwatch.sh

--- lab6/src/main/java/lab6/Server.java
+++ lab6/src/main/java/lab6/Server.java
@@ -3,33 +3,38 @@ package lab6;
 import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
 
 import java.time.Duration;
+import java.util.concurrent.CompletionStage;
 
 import static akka.http.javadsl.server.Directives.*;
 
 public class Server {
     private static final String URL = "url";
     private static final String COUNT = "count";
-    private Http http;
+    private static Http http;
     private ActorRef storeActor;
     private Duration timeout = Duration.ofSeconds(5);
     public Server(Http http, ActorRef storeActor){
         this.http = http;
         this.storeActor = storeActor;
     }
+    private static CompletionStage<HttpResponse> fetch(String url){
+        return http.singleRequest(HttpRequest.create(url));
+    }
     public Route createRoute(){
         return route(get() ->
             parameter(URL, (url) ->
                     parameter(COUNT, count -> {
                         if (Integer.parseInt(count) <= 0){
-                            return completeWithFuture(http.singleRequest(HttpRequest.create(url)));
+                            return completeWithFuture(fetch(url));
                         }
                         return completeWithFuture(Patterns.ask(this.storeActor, new RandomServer(), timeout))
-                                .thenCompose((serverUrl)
-
+                                .thenCompose((serverUrl) ->
+                                    fetch()
                                 )
                     })
 
