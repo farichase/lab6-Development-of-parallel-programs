d29609b3512c5e2ba5f427733f7ab540a3e6484a
farichase <fbazartinova@mail.ru>
2020-12-18 20:03:54 +0300

    Scripted auto-commit on change (2020-12-18 20:03:54) by gitwatch.sh

--- lab6/src/main/java/lab6/Message.java
+++ lab6/src/main/java/lab6/Message.java
@@ -1,4 +1,13 @@
 package lab6;
 
+import java.util.ArrayList;
+
 public class Message {
+    private final ArrayList<String> servers;
+    public Message(ArrayList<String> servers) {
+        this.servers = servers;
+    }
+    public ArrayList<String> getServers(){
+        return this.servers;
+    }
 }
