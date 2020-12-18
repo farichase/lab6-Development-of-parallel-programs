f5ec40315c275b59b2ec6d02a978299fe6fa98ba
farichase <fbazartinova@mail.ru>
2020-12-18 20:26:47 +0300

    Scripted auto-commit on change (2020-12-18 20:26:47) by gitwatch.sh

--- lab6/src/main/java/lab6/Server.java
+++ lab6/src/main/java/lab6/Server.java
@@ -2,12 +2,20 @@ package lab6;
 
 import akka.http.javadsl.server.Route;
 
+import static akka.http.javadsl.server.Directives.parameter;
 import static akka.http.javadsl.server.Directives.route;
 
 public class Server {
+    private static final String URL;
+    public Server(String URL){
+        this.URL = URL;
+    }
     public Route createRoute(){
         return route(get() ->
-        
+            parameter(URL, url ->
+                    parameter()
+
+            )
 
         );
     }
