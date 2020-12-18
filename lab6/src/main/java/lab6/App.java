b201c3cef1cfd9a17f7e298b99730eeb01720890
farichase <fbazartinova@mail.ru>
2020-12-18 20:07:40 +0300

    Scripted auto-commit on change (2020-12-18 20:07:40) by gitwatch.sh

--- lab6/src/main/java/lab6/App.java
+++ lab6/src/main/java/lab6/App.java
@@ -1,7 +1,9 @@
 package lab6;
 
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
@@ -19,6 +21,7 @@ public class App {
     public static void main(String[] args) throws IOException {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("routes");
+        ActorRef storeActor = system.actorOf(Props.create(StoreActor.class))
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = new ;
--- lab6/src/main/java/lab6/StoreActor.java
+++ lab6/src/main/java/lab6/StoreActor.java
@@ -3,12 +3,18 @@ package lab6;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.ArrayList;
+
 public class StoreActor extends AbstractActor {
+    private ArrayList<String> servers;
     public Receive createReceive(){
         return ReceiveBuilder.create()
                 .match(Message.class, msg -> {
-
+                    this.servers = msg.getServers();
                 })
+                .match(
+
+                ).build();
 
     }
 }
