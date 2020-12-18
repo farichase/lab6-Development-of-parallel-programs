ed2301c815cea4ea9b2e278d78f9851b0aef37c1
farichase <fbazartinova@mail.ru>
2020-12-18 19:40:09 +0300

    Scripted auto-commit on change (2020-12-18 19:40:02) by gitwatch.sh

--- lab6/src/main/java/lab6/App.java
+++ lab6/src/main/java/lab6/App.java
@@ -1,8 +1,35 @@
 package lab6;
 
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
 public class App {
-    public static void main(String[] args){
+    private final static String HOST = "localhost";
+    private final static int PORT = 8080;
+    public static void main(String[] args) throws IOException {
         System.out.println("start!");
-        A
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = new ;
+        final CompletionStage<ServerBinding> bindingCompletionStage = http.bindAndHandle(
+            routeFlow,
+            ConnectHttp.toHost(HOST, PORT),
+            materializer
+        );
+        System.out.println("Server online at http://localhost:8080");
+        System.in.read();
+        bindingCompletionStage.thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 }
