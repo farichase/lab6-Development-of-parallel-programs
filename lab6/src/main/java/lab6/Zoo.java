ae3c6e3b6df4cf0707b502441e99cc8595bc640d
farichase <fbazartinova@mail.ru>
2020-12-19 18:34:43 +0300

    Scripted auto-commit on change (2020-12-19 18:34:41) by gitwatch.sh

--- lab6/src/main/java/lab6/Zoo.java
+++ lab6/src/main/java/lab6/Zoo.java
@@ -1,7 +1,22 @@
 package lab6;
 
 import akka.actor.ActorRef;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
 
 public class Zoo {
-    public Zoo(ActorRef )
+    private ZooKeeper zooKeeper;
+    private String CONNECT_STRING = "127.0.0.1:2181";
+    private int timeout = 3000;
+    private ActorRef storeActor;
+    public Zoo(ActorRef storeActor) throws IOException {
+        this.zooKeeper = new ZooKeeper(CONNECT_STRING, timeout, (Watcher) null);
+        this.storeActor = storeActor;
+
+    }
+    private void serverWatch(){
+        
+    }
 }
