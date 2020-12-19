f9f160027124ecd00b1dd6bff6e7860f5f03216c
farichase <fbazartinova@mail.ru>
2020-12-19 21:05:46 +0300

    Scripted auto-commit on change (2020-12-19 21:05:46) by gitwatch.sh

--- lab6/src/main/java/lab6/Zoo.java
+++ lab6/src/main/java/lab6/Zoo.java
@@ -12,30 +12,34 @@ public class Zoo {
     private final String CONNECT_STRING = "127.0.0.1:2181";
     private final int timeout = 3000;
     private ActorRef storeActor;
-    public Zoo(ActorRef storeActor, int port) throws IOException, KeeperException, InterruptedException {
+    public Zoo(ActorRef storeActor, int port) throws IOException {
         this.zooKeeper = new ZooKeeper(CONNECT_STRING, timeout, watcher);
         this.storeActor = storeActor;
+    }
+    public void createServer(int port) throws KeeperException, InterruptedException{
         this.zooKeeper.create("/servers/" + port, String.valueOf(port).getBytes(),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
         WatchedEvent event = new WatchedEvent(Watcher.Event.EventType.NodeCreated,
                 Watcher.Event.KeeperState.SyncConnected, "");
         watcher.process(event);
     }
-    private Watcher watcher = watchedEvent -> {
+
+    public Watcher watcher = watchedEvent -> {
         if (watchedEvent.getType() == Watcher.Event.EventType.NodeCreated ||
-            watchedEvent.getType() == Watcher.Event.EventType.NodeDeleted ||
-            watchedEvent.getType() == Watcher.Event.EventType.NodeDataChanged){
+                watchedEvent.getType() == Watcher.Event.EventType.NodeDeleted ||
+                watchedEvent.getType() == Watcher.Event.EventType.NodeDataChanged) {
             ArrayList<String> serversNames = new ArrayList<>();
             try {
                 Iterator iterator = this.zooKeeper.getChildren("/servers", null).iterator();
-                while(iterator.hasNext()){
+                while (iterator.hasNext()) {
                     String line = (String) iterator.next();
                     byte[] serverUrl = this.zooKeeper.getData("/servers/" + line, false, null);
                     serversNames.add(new String(serverUrl));
                 }
                 this.storeActor.tell(new Message(serversNames), ActorRef.noSender());
-            } catch(KeeperException | InterruptedException e) {
+            } catch (KeeperException | InterruptedException e) {
                 e.printStackTrace();
             }
+        }
     }
 }
