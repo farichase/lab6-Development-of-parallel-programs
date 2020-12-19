3f2cf812d11453a62ca747d535bd20da7f4b0455
farichase <fbazartinova@mail.ru>
2020-12-19 18:43:48 +0300

    Scripted auto-commit on change (2020-12-19 18:43:48) by gitwatch.sh

--- lab6/src/main/java/lab6/Zoo.java
+++ lab6/src/main/java/lab6/Zoo.java
@@ -1,14 +1,19 @@
 package lab6;
 
 import akka.actor.ActorRef;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Zoo {
     private ZooKeeper zooKeeper;
     private String CONNECT_STRING = "127.0.0.1:2181";
+    private String PATH = "/servers";
     private int timeout = 3000;
     private ActorRef storeActor;
     public Zoo(ActorRef storeActor) throws IOException {
@@ -18,8 +23,19 @@ public class Zoo {
     }
     private void serverWatch(){
         try {
-            
-        } catch() {
+            List<String> serversChildren = this.zooKeeper.getChildren(PATH, watchedEvent -> {
+                if (watchedEvent.getType() == Watcher.Event.EventType.NodeChildrenChanged){
+                    this.serverWatch();
+                }
+            });
+            ArrayList<String> serversNames = new ArrayList<>();
+            Iterator iterator = serversChildren.iterator();
+
+            while(iterator.hasNext()){
+                String line = (String) iterator.next();
+                
+            }
+        } catch(KeeperException | InterruptedException e) {
 
         }
     }
