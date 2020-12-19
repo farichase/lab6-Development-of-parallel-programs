991b68491eedf5a80e293e5cea27b0af062bf7ed
farichase <fbazartinova@mail.ru>
2020-12-19 19:26:45 +0300

    Scripted auto-commit on change (2020-12-19 19:26:45) by gitwatch.sh

--- lab6/src/main/java/lab6/StoreActor.java
+++ lab6/src/main/java/lab6/StoreActor.java
@@ -1,9 +1,11 @@
 package lab6;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class StoreActor extends AbstractActor {
     private ArrayList<String> servers;
@@ -13,8 +15,8 @@ public class StoreActor extends AbstractActor {
                     this.servers = msg.getServers();
                 })
                 .match(RandomServer.class, msg -> {
-
-                    getSender().tell(servers.);
+                    Random rand = new Random();
+                    getSender().tell(servers.get(rand.nextInt(servers.size())), ActorRef.noSender());
                 })
         .build();
 
