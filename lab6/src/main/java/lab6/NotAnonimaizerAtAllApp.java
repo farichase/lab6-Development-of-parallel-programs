58db4c8142e3fc023eac19ab0559ff42b863851d
farichase <fbazartinova@mail.ru>
2020-12-21 14:46:41 +0300

    Scripted auto-commit on change (2020-12-21 14:46:40) by gitwatch.sh

--- lab6/src/main/java/lab6/NotAnonimaizerAtAllApp.java
+++ lab6/src/main/java/lab6/NotAnonimaizerAtAllApp.java
@@ -25,6 +25,8 @@ public class NotAnonimaizerAtAllApp {
         ActorRef storeActor = system.actorOf(Props.create(StoreActor.class), "storeActor");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Zoo zoo = new Zoo(zoo, storeActor);
+        
         final Server server = new Server(http, storeActor, PORT);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.createRoute().flow(system, materializer) ;
         final CompletionStage<ServerBinding> bindingCompletionStage = http.bindAndHandle(
